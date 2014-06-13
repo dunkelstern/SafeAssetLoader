3f08ecd01005146f55ade0ed63aba1a3813c9cf0
Johannes Schriewer <js@planetmutlu.com>
2014-06-13 16:31:11 +0200

    Add header documentation for ALAssetRepresentation+SafeLoading

--- AssetLoader/ALAssetRepresentation+SafeLoading.h
+++ AssetLoader/ALAssetRepresentation+SafeLoading.h
@@ -10,9 +10,20 @@
 
 @interface ALAssetRepresentation (SafeLoading)
 
+/** @brief load the asset representation in reduced size to save RAM
+ *
+ *  @param[in] maxLongSide maximum length of the long side of the asset
+ *  @returns new CGImageRef with loaded asset, all EXIF and XMP adjustments
+ *           applied in upright rotation. Image has to be freed by caller.
+ */
 - (CGImageRef)loadImageOfSize:(CGFloat)maxLongSide CF_RETURNS_RETAINED;
 @end
 
 @interface NSDictionary (AssetLoading)
+/** @brief calculate the size of the original asset from a metadata dictionary
+ *
+ *  @returns pixel size of the asset this dictionary belongs to after all
+ *           EXIF rotations and XMP adjustments have been applied.
+ */
 - (CGSize)assetSize;
 @end
